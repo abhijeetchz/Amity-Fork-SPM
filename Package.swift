fcd5ff10c8552683066518b5a6ad527fe0e91796
Abhijeet <Sunny@Appunites-MacBook-Pro-2.local>
2024-07-10 13:30:12 +0200

    updated package

--- Package.swift
+++ Package.swift
@@ -6,18 +6,23 @@ import PackageDescription
 let package = Package(
     name: "ZumbaAmityKit",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZumbaAmityKit",
-            targets: ["ZumbaAmityKit"]),
+            targets: ["ZumbaAmityKit"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "AmitySDK",
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.20.7/AmitySDK.xcframework.zip",
+            checksum: "64fa391f50185357e99f2a7808e293638a34f4d729defc7eba669b66c0da8fcd"
+        ),
         .target(
-            name: "ZumbaAmityKit"),
+            name: "ZumbaAmityKit"
+        ),
         .testTarget(
             name: "ZumbaAmityKitTests",
-            dependencies: ["ZumbaAmityKit"]),
+            dependencies: ["ZumbaAmityKit"]
+        ),
     ]
 )
