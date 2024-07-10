882b6d1e0c89bee51e4a6ecdc569fbdcf5b89748
Abhijeet <Sunny@Appunites-MacBook-Pro-2.local>
2024-07-10 17:39:02 +0200

    updated package

--- Package.swift
+++ Package.swift
@@ -16,11 +16,6 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "AmityUIKit",
-            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.20.7/AmityUIKit.xcframework.zip",
-            checksum: "081f8a47d910e50a712375f4b9dc659ec8e4feec9d1d142857fee3e51e1e0105"
-        ),
-        .binaryTarget(
             name: "AmitySDK",
             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.20.7/AmitySDK.xcframework.zip",
             checksum: "64fa391f50185357e99f2a7808e293638a34f4d729defc7eba669b66c0da8fcd"
@@ -37,8 +32,7 @@ let package = Package(
         ),
         .target(
             name: "ZumbaAmityKit",
-            dependencies: ["AmityUIKit", "AmitySDK", "Realm", "RealmSwift"],
-            resources: [.process("Modules")]
+            dependencies: ["AmitySDK", "Realm", "RealmSwift"]
         ),
         .testTarget(
             name: "ZumbaAmityKitTests",
